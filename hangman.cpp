705a0b5656c7ed6e0be5d4903528de6300db0e1c
Benjami Ahn <benjaminsolbiahn@gmail.com>
2019-09-30 13:15:49 -0700

    a basic game of hang man in the works

--- hangman.cpp
+++ hangman.cpp
@@ -7,15 +7,13 @@ int main() {
       std::string guessing_word = "";
       std::string guessing_word_ = "";
       int length_of_word = 0;
-      int guess_counter = 0, guess_max = 6;
+      int guess_counter = 0, guess_max = 6, guess_finder = 0;
       char guess;
 
     std::cout << "Select a word to find. \n";
     std::cin >> guessing_word;
     length_of_word = guessing_word.length();
-    guessing_word_ = //trying to assign a number of _ equal to length of the word
-
-    }
+    //trying to assign a number of _ equal to length of the word
 
     char guessing_word_array[length_of_word + 1];
     strcpy(guessing_word_array, guessing_word.c_str());
@@ -23,10 +21,12 @@ int main() {
     std::cout << "Guess a letter \n";
 
 while (guess_counter < 6) {
+
   std::cin >> guess;
-    if (guessing_word.find(guess) == true) {
-      for (i = 0; i < length_of_word; i++)
-        //find letter inside the string if it exists
+    if (guessing_word.find(guess) == true) { //find returns the location, use find to find location of guess and use str.find/str.push_back to replace _ with correct guesses
+      for (guess_finder = 0; guess_finder < length_of_word; guess_finder++)
+        //return the correct number of _ with the 'guess' inserted in appropriate locations
+        ;
     }
     else {
       guess_counter ++;
@@ -39,6 +39,8 @@ while (guess_counter < 6) {
 
 /*for (int i = 0; i < length_of_word; i++)
   std::cout << guessing_word_array[i] << "\n";
+
+  std::string actualGuess;  //--> _________
 */
 
 /*
@@ -48,6 +50,7 @@ set it up for two players
 animate an actual hangman (possible?)
 display letters already used
 enable guessing words
+look up maps
 */
 
   return 0;
